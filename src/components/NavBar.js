e88f3fe4b29a6024ec57872180865478f300c114
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-15 17:17:53 +0100

    feat: adjust NavBar for login/logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,85 @@ import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import { axiosReq } from "../api/axiosDefaults";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleLogOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  const addPaintingIcon = (
+    <NavLink
+      style={{ textTransform: "none" }}
+      to="/paintings/create"
+      className={`${styles.NavLink} ${styles.NavLinkSpacing}`}
+      activeClassName={styles.Active}
+    >
+      <span className={styles.IconTextContainer}>
+        <i className="fas fa-plus-square"></i>Add painting
+      </span>
+    </NavLink>
+  );
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        to="/favourites"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        faves
+      </NavLink>
+
+      <NavLink
+        to="/watchlist"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        watchlist
+      </NavLink>
+
+      <NavLink to="/" onClick={handleLogOut} className={styles.NavLink}>
+        log out
+      </NavLink>
+
+      <NavLink
+        to="/about"
+        className={`${styles.NavLink} ${styles.NavLinkSpacing}`}
+        activeClassName={styles.Active}
+      >
+        about
+      </NavLink>
+
+      <NavLink
+        style={{ textTransform: "none" }}
+        to={`/profiles/${currentUser?.profile_id}`}
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <Avatar
+          src={currentUser?.profile_image}
+          text={currentUser?.username}
+          // text="Profile" // only fo testing!!!
+          height={40}
+        />
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -19,7 +91,7 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
       >
-        REGISTER
+        register
       </NavLink>
       <NavLink
         exact
@@ -27,19 +99,20 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
       >
-        LOGIN
+        login
       </NavLink>
     </>
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="lg" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="35" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addPaintingIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto text-left">
@@ -52,14 +125,6 @@ const NavBar = () => {
               ARTWORKS
             </NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
-            <NavLink
-              exact
-              to="/about"
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-            >
-              ABOUT
-            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
