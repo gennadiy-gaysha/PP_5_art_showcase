1b37a8cf9097d4d0188dd2a64fc1efda52799ca6
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-17 16:10:37 +0100

    feat: set up boilerplate for paintings list

--- src/App.js
+++ src/App.js
@@ -8,15 +8,46 @@ import RegistrationForm from "./pages/auth/RegistrationForm";
 import LogInForm from "./pages/auth/LogInForm";
 import PaintingCreateForm from "./pages/paintings/PaintingCreateForm";
 import PaintingPage from "./pages/paintings/PaintingPage";
+import PaintingsPage from "./pages/paintings/PaintingsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
 
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Artworks</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PaintingsPage message="No results found. Adjust your search options." />
+            )}
+          />
+          <Route
+            exact
+            path="/favourites"
+            render={() => (
+              <PaintingsPage
+                message="No results found. Adjust the search keyword or add an artist to Faves."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/watchlist"
+            render={() => (
+              <PaintingsPage
+                message="No results found. Adjust the search keyword or click on the eye under the painting to follow it up."
+                filter={`'observations__owner__profile'=${profile_id}&ordering=-observationss__created_at&`}
+              />
+            )}
+          />
           <Route
             exact
             path="/registration"
---
+++ src/pages/paintings/PaintingsPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PaintingsPage.module.css";
+
+function PaintingsPage() {
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>List of posts here</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PaintingsPage;
