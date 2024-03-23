e88e316352356c5c417aedc6596d5909adc1c93d
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-23 16:57:15 +0100

    feat: add profile data and styles to the ProfilePage

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -48,60 +48,71 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
-      <Row noGutters className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
-          <Image
-            className={styles.ProfileImage}
-            roundedCircle
-            src={profile?.image}
-          />
+      <Row noGutters className="px-3 text-left">
+        <Col lg={2} className="text-lg-center">
+          <Col md={12} className="text-center">
+            <Image
+              className={styles.ProfileImage}
+              roundedCircle
+              src={profile?.image}
+            />
+          </Col>
+
+          <Col md={12} className="text-center mb-5">
+            {currentUser &&
+              !is_owner &&
+              (profile?.following_id ? (
+                <Button
+                  className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+                  onClick={() => {}}
+                >
+                  Unfollow
+                </Button>
+              ) : (
+                <Button className={styles.Button} onClick={() => {}}>
+                  to Faves
+                </Button>
+              ))}
+          </Col>
         </Col>
-        <Col lg={6}>
-          <h3 className="m-2">{profile?.owner}</h3>
-          <Row className="justify-content-center no-gutters">
-            <Col xs={3} className="my-2">
-              <div>{profile?.paintings_count}</div>
+        <Col lg={10}>
+          <h3 className="ml-3">{profile?.name}</h3>
+          {profile?.bio && <Col className="p-3">{profile.bio}</Col>}
+          <Row className="justify-content-center no-gutters text-center">
+            <Col lg={5} className="text-left p-3">
+              <div className={styles.profileBody}>
+                <p>HOME COUNTRY: {profile?.home_country}</p>
+                <p>GENDER: {profile?.gender}</p>
+                <p>
+                  DATE OF BIRTH:{" "}
+                  {new Date(profile?.birthdate).toLocaleDateString()}
+                </p>
+                <p>E-MAIL: {profile?.email}</p>
+              </div>
+            </Col>
+            <Col lg={2} md={4} xs={4} className="my-2">
               <div>paintings</div>
+              <div>{profile?.paintings_count}</div>
             </Col>
-            <Col xs={3} className="my-2">
-              <div>{profile?.followers_count}</div>
+            <Col lg={2} md={4} xs={4} className="my-2">
               <div>followers</div>
+              <div>{profile?.followers_count}</div>
             </Col>
-            <Col xs={3} className="my-2">
-              <div>{profile?.following_count}</div>
+            <Col lg={2} md={4} xs={4} className="my-2">
               <div>faves</div>
+              <div>{profile?.following_count}</div>
             </Col>
+            <Col lg={1}></Col>
           </Row>
         </Col>
-        <Col lg={3} className="text-lg-right">
-          {currentUser &&
-            !is_owner &&
-            (profile?.following_id ? (
-              <Button
-                className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
-                onClick={() => {}}
-              >
-                unfollow
-              </Button>
-            ) : (
-              <Button
-                className={`${btnStyles.Button} ${btnStyles.Black}`}
-                onClick={() => {}}
-              >
-                follow
-              </Button>
-            ))}
-        </Col>
-        {profile?.content && <Col className="p-3">{profile.content}</Col>}
+        <Col lg={3} className="text-lg-right"></Col>
       </Row>
     </>
   );
 
   const mainProfilePaintings = (
     <>
-      <hr />
-      <p className="text-center">Profile owner's paintings</p>
-      <hr />
+      <h4 className="text-left p-4 ml-4">AVAILABLE ARTWORKS:</h4>
     </>
   );
 
@@ -109,15 +120,9 @@ function ProfilePage() {
     <Row>
       <Col className="py-2 p-0 p-lg-2" lg={12}>
         <Container className={appStyles.Content}>
-          {hasLoaded ? (
-            <>
-              {mainProfile}
-              {mainProfilePaintings}
-            </>
-          ) : (
-            <Asset spinner />
-          )}
+          {hasLoaded ? <>{mainProfile}</> : <Asset spinner />}
         </Container>
+        {mainProfilePaintings}
       </Col>
     </Row>
   );
